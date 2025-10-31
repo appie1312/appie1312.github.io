7a26387a62d775299cb63bed28dd3d49aec4565b
appie1312 <agoeswag@gmil.com>
2025-10-31 15:12:17 +0100

    github fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // user site (appie1312.github.io) moet base op '/' hebben
   plugins: [react()],
 })
